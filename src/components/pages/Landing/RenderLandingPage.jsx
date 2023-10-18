4681db8206cd381655271218ab07bb2797e00a8f
Michael Newell <michael.newell212@gmail.com>
2023-10-18 16:04:14 -0600

    added images to render landing page

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -33,6 +33,23 @@ function RenderLandingPage(props) {
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
       {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <img
+          src={GrantRatesByOfficeImg}
+          alt="Grant Rates By Office"
+          className="by-office-img"
+        />
+        <img
+          src={GrantRatesByNationalityImg}
+          alt="Grant Rates by Nationality"
+          className="by-nationality-img"
+        />
+        <img
+          src={GrantRatesOverTimeImg}
+          alt="Grant Rates Over time"
+          className="over-time-img"
+        />
+      </div>
       <div className="view-more-data-btn-container">
         <Button
           type="default"
